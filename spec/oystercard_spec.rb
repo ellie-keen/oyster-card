bc525b416b275859d016cf0197f1d2473ce381d7
Ellie Keen <elliekeen95@gmail.com>
2018-04-23 21:15:52 +0100

    Renamed test descriptions and tidied up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,38 +1,38 @@
 require 'oystercard'
 
 describe Oystercard do
-  let(:oystercard) { Oystercard.new }
+let(:oystercard) { Oystercard.new }
 
   describe ' #balance ' do
-    it 'should return the balance' do
+    it 'should initialize card with a balance of 0' do
       expect(oystercard.balance).to eq 0
     end
   end
 
   describe ' #top_up ' do
-    it 'should respond to top_up method' do
+    it 'should take an amount as an argument' do
       expect(oystercard).to respond_to(:top_up).with(1).argument
     end
 
-    it 'should return correct balance when topped up' do
+    it 'should add the amount to the balance' do
       expect { oystercard.top_up(10) }.to change { oystercard.balance }.by(10)
     end
 
-    it 'should raise error if total exceeds max limit' do
+    it 'raises error if it exceeds the maximum limit' do
       expect { oystercard.top_up(100) }.to raise_error(RuntimeError,
-        'Balance exceeds limit.')
+      'Balance exceeds limit.')
     end
   end
 
   describe ' #deduct ' do
-    it 'should deduct an amount from our balance and return correct amount' do
+    it 'should deduct amount from balance' do
       oystercard.instance_variable_set(:@balance, 20)
       expect { oystercard.deduct(10) }.to change { oystercard.balance }.by(-10)
     end
 
-    it 'should raise error if balance is below 0' do
+    it 'raises error if balance is below minimum' do
       expect { oystercard.deduct(10) }.to raise_error(RuntimeError,
-        'Cannot travel. Insufficent funds.')
+      'Cannot travel. Insufficent funds.')
     end
-end
+  end
 end
