33f9cdb86c9cb28b46d57471c91b586ef0fdf946
Charlene <charlenechetcuti@gmail.com>
2018-04-26 14:29:26 +0100

    passed test for initialsing journey is complete to false

--- lib/journey.rb
+++ lib/journey.rb
@@ -4,10 +4,15 @@ attr_reader :entry_station
 
   def initialize(entry_station: nil)
     @entry_station = entry_station
+    @complete = false 
   end
 
   def set_entry_station(station)
     @entry_station = station
+  end
+
+  def complete?
+    @complete
   end
 
 end
